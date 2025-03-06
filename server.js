d1d86af6a3713a9f245a15d5fd2b4a1cca081218
oxyum services <29GariglioSe@fortcherry.org>
2025-03-06 02:53:03 -0500

    push

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files (HTML, CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Send the index.html file for any GET request
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});
